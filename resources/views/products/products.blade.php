a72f171eed6d30a4327509c4bb884d031deb7896
Antoine Gaudry <antoinegaudry@Ordinateur-portable-de-Antoine.local>
2024-06-17 17:08:03 +0200

    test

---
+++ resources/views/products/products.blade.php
@@ -0,0 +1,77 @@
+@include('components.header')
+<div class="d-flex flex-column container w-100">
+    <span class="fw-medium mt-2 mb-4">Produits :</span>
+    <div class="d-flex flex-row flex-wrap w-100">
+        <div class="cardItem">
+            <img src="{{asset('chaise.webp')}}" class="cardItem-img" alt="">
+            <div class="d-flex flex-row align-items-center justify-content-between w-100 mt-4">
+                <div class="d-flex flex-column">
+                    <span class="cardItem-title">Titre</span>
+                    <span class="cardItem-subtitle">45.34</span>
+                </div>
+                <button class="btn btn-primary">
+                    +
+                    <i class="fa-solid fa-cart-shopping ms-2"></i>
+                </button>
+            </div>
+            <span class="cardItem-subtitle mt-3 text-secondary">sous titre test kela e ekf ekf eofo efke okfoek foe</span>
+        </div>
+        <div class="cardItem">
+            <img src="{{asset('chaise.webp')}}" class="cardItem-img" alt="">
+            <div class="d-flex flex-row align-items-center justify-content-between w-100 mt-4">
+                <div class="d-flex flex-column">
+                    <span class="cardItem-title">Titre</span>
+                    <span class="cardItem-subtitle">45.34</span>
+                </div>
+                <button class="btn btn-primary">
+                    +
+                    <i class="fa-solid fa-cart-shopping ms-2"></i>
+                </button>
+            </div>
+            <span class="cardItem-subtitle mt-3 text-secondary">sous titre test kela e ekf ekf eofo efke okfoek foe</span>
+        </div>
+        <div class="cardItem">
+            <img src="{{asset('chaise.webp')}}" class="cardItem-img" alt="">
+            <div class="d-flex flex-row align-items-center justify-content-between w-100 mt-4">
+                <div class="d-flex flex-column">
+                    <span class="cardItem-title">Titre</span>
+                    <span class="cardItem-subtitle">45.34</span>
+                </div>
+                <button class="btn btn-primary">
+                    +
+                    <i class="fa-solid fa-cart-shopping ms-2"></i>
+                </button>
+            </div>
+            <span class="cardItem-subtitle mt-3 text-secondary">sous titre test kela e ekf ekf eofo efke okfoek foe</span>
+        </div>
+        <div class="cardItem">
+            <img src="{{asset('chaise.webp')}}" class="cardItem-img" alt="">
+            <div class="d-flex flex-row align-items-center justify-content-between w-100 mt-4">
+                <div class="d-flex flex-column">
+                    <span class="cardItem-title">Titre</span>
+                    <span class="cardItem-subtitle">45.34</span>
+                </div>
+                <button class="btn btn-primary">
+                    +
+                    <i class="fa-solid fa-cart-shopping ms-2"></i>
+                </button>
+            </div>
+            <span class="cardItem-subtitle mt-3 text-secondary">sous titre test kela e ekf ekf eofo efke okfoek foe</span>
+        </div>
+        <div class="cardItem">
+            <img src="{{asset('chaise.webp')}}" class="cardItem-img" alt="">
+            <div class="d-flex flex-row align-items-center justify-content-between w-100 mt-4">
+                <div class="d-flex flex-column">
+                    <span class="cardItem-title">Titre</span>
+                    <span class="cardItem-subtitle">45.34</span>
+                </div>
+                <button class="btn btn-primary">
+                    +
+                    <i class="fa-solid fa-cart-shopping ms-2"></i>
+                </button>
+            </div>
+            <span class="cardItem-subtitle mt-3 text-secondary">sous titre test kela e ekf ekf eofo efke okfoek foe</span>
+        </div>
+    </div>
+</div>
+@include('components.footer')
