26d9a96c193bb38a38077e800075abe5dba5b27f
Quentin <qgransart@goweb.fr>
2024-06-17 16:26:02 +0200

    feat/creation-formulaire-contact

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
